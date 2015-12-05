d83b5784754f41e4133bc1cb889405b4eb32402c
Martin Wiegand <dm@freibrief.net>
2015-12-05 11:56:47 +0100

    random commit

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
   resources :users, shallow: true do
     resources :user_roles
   end
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :permissions
   resources :roles, shallow: true do
